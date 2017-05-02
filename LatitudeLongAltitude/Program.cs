79b1688ba6c358dc994b2f5af96e7e2004c1a4f7
Argyll <argyllmcghie@gmail.com>
2017-05-02 15:54:12 +1000

    added power hubs to the data pack
    Can also display substations

--- LatitudeLongAltitude/Program.cs
+++ LatitudeLongAltitude/Program.cs
@@ -11,6 +11,12 @@ namespace LatitudeLongAltitude
 {
     internal class Program
     {
+
+        // Todo private string Path = Environment.CurrentDirectory;
+
+        
+
+
         private static IEnumerable<string[]> LoadCsvData(string path, params char[] separator)
         {
             return from line in File.ReadLines(path)
@@ -68,17 +74,6 @@ namespace LatitudeLongAltitude
                 return latlon500;
             }
 
-            /*
-
-            string[] ele = elevation.Split('[');
-
-            string[] elevations = ele[2].Split(']');
-            Console.Write(ele[0]);
-            Console.WriteLine("ALt" + elevations[0]);
-
-
-            return elevations[0];
-            */
         }
 
 
@@ -277,15 +272,189 @@ namespace LatitudeLongAltitude
         {
         }
 
+        //need to enter the location of the latitude, longitude and elevation (elavtion keep at the end after longitude.)
+        //this should send the information to the "retrieve the elevation from the google api program"
+
+        // var headinglist = [OBJECTID,FEATURETYPE,NAME,CAPACITY_kV,CLASSIFICATION,SITESUBURB,STATE,STATUS,FEATURERELIABILITY,FEATURESOURCE,ATTRIBUTERELIABILITY,ATTRIBUTESOURCE,SPATIALACCURACY,REVISED,LATITUDE,LONGITUDE]
+
+
+
+
+        private static void elavationFromcsv()
+        {
+            var filename = @"C:\Users\blue\Desktop\New folder\elec.csv";
+            var filenameNew = @"C:\Users\blue\Desktop\New folder\elec_Allmost.json";
+            var split = ',';
+
+
+            
+
+            var postcode = "";
+            var suburb = "";
+            var lat = "1";
+            var lon = "1";
+
+
+            var latlon = "1,1";
+            var latlon500 = "";
+            var counter = 0;
+            var totalElevations = "";
+            var simpleCounter = 1;
+
+
+            var lines = LoadCsvData(filename, ',');
+            var Allentry = "";
+
+            if (lines != null)
+            {
+                var howmanyline = lines.Count();
+
+                foreach (var line in lines)
+                {
+                    try
+                    {
+                      //  postcode = line[0].Replace('"', ' ');
+                       // suburb = line[1].Replace('"', ' ');
+                        lat = line[14].Replace('"', ' ');
+                        lon = line[15].Replace('"', ' ');
+                        latlon = lat + ',' + lon;
+                    }
+                    catch (Exception e)
+                    {
+                    }
+
+
+                    counter++;
+                    simpleCounter++;
+                    if (simpleCounter == howmanyline)
+                    {
+                        latlon500 = latlon500 + latlon;
+
+                        var elv = Altitude(latlon500);
+                        totalElevations = totalElevations + elv;
+                        File.WriteAllText(filenameNew, totalElevations);
+                    }
+
+                    else if ((counter <= 100) && (simpleCounter != howmanyline))
+                    {
+                        latlon500 = latlon500 + latlon + '|';
+                    }
+                    else if (counter == 101)
+                    {
+                        latlon500 = latlon500 + latlon;
+
+                        var elv = Altitude(latlon500);
+                        totalElevations = totalElevations + elv;
+                        File.WriteAllText(filenameNew, totalElevations);
+                        latlon500 = "";
+                        counter = 0;
+                    }
+                }
+            }
+        }
+
+        private static void Nationalpowersubstations()
+        {
+            var filename = @"C:\Users\blue\Desktop\New folder\elec.csv";
+            var googelelvationF = @"C:\Users\blue\Desktop\New folder\elec_Allmost.json";
+
+            var filenameNew = @"C:\Users\blue\Desktop\New folder\elec_data.json";
+
+            var filenameSmall = @"C:\Users\blue\Desktop\New folder\elec_datasmall.js";
+
+            var split = ',';
+
+            var OBJECTID = "";
+            var FEATURETYPE = "";
+            var NAME = "";
+            var CAPACITY_kV = "";
+            var CLASSIFICATION = "";
+            var SITESUBURB = "";
+            var STATE = "";
+            var STATUS = "";
+            var FEATURERELIABILITY = "";
+            var FEATURESOURCE = "";
+            var ATTRIBUTERELIABILITY = "";
+            var ATTRIBUTESOURCE = "";
+            var SPATIALACCURACY = "";
+            var REVISED = "";
+            var LATITUDE = "1";
+            var LONGITUDE = "1";
+            ///////////////////
+            var lat = "1";
+            var lon = "1";
+            var latlon = "1,1";
+            var count = 0;
+            var ele = "1";
+           var elevationList = readJsonfile(googelelvationF);
+
+            var listsize = elevationList.Count;
+
+
+            var lines = LoadCsvData(filename, ',');
+            var Allentry = "";
+            var SmallEntry = "var locations = [";
+
+
+            if (lines != null)
+            {
+                var howmanyline = lines.Count();
+
+                foreach (var line in lines)
+                {
+                    try
+                    {
+                        OBJECTID = line[0].Replace('"', ' ');
+                        CAPACITY_kV = line[3].Replace('"', ' ');
+                        lat = line[14].Replace('"', ' ');
+                        lon = line[15].Replace('"', ' ');
+                      ele = elevationList[count++];
+                    }
+                    catch (Exception e)
+                    {
+                    }
+
+                    try
+                    {
+                        var small = '[' + OBJECTID + ',' + '"' + CAPACITY_kV + '"' + ',' + lat + ',' + lon + ',' + ele + "],";
+                        SmallEntry = SmallEntry + small;
+
+                        File.WriteAllText(filenameSmall, SmallEntry);
+                        Console.Write(small);
+                    }
+                    catch (Exception e)
+                    {
+                    }
+
+
+                    var entry = '"' + "Location" + '"' + " : {" +
+                                "ID" + '"' + ": " + '"' + OBJECTID + '"' + ',' +
+                                " CP_Kv" + '"' + ": " + '"' + '"' + CAPACITY_kV + '"' + '"' + ',' +
+                                " Latitude" + '"' + ": " + '"' + lat + '"' + ',' +
+                                " Longitude" + '"' + ": " + '"' + lon + '"' + ',' +
+                                " Altitude" + '"' + ": " + '"' + ele + '"' + "},\n{";
+
+                    //   Console.Write(entry);
+
+                           Allentry = Allentry + entry;
+                }
+                     File.WriteAllText(filenameNew, Allentry);
+            }
+
+
+
+        }
+
 
         private static void Main(string[] args)
         {
             //            var filename = @"C:\Users\blue\Desktop\New folder\entriesAllmost.json";
             //   getthedata();
 
-            createfile();
+            //createfile();
+            Nationalpowersubstations();
 
-
+           // elavationFromcsv();
             /*
             try
             {
